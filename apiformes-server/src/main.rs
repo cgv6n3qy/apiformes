ba4e9438b0d44a48374124fdbd0d19c8c4dc6a3a
cgv6n3qy <apiformes@protonmail.com>
2021-12-19 11:32:55 +0000

    Fix minor bug in incomplete main function
    Signed-off-by: cgv6n3qy <apiformes@protonmail.com>

--- apiformes-server/src/main.rs
+++ apiformes-server/src/main.rs
@@ -1,5 +1,4 @@
 use apiformes::server_async::{MqttServer, MqttServerConfig};
-use std::net::SocketAddr;
 use tracing_subscriber::FmtSubscriber;
 #[tokio::main]
 async fn main() {
@@ -7,6 +6,7 @@ async fn main() {
     tracing::subscriber::set_global_default(sub).expect("setting tracing default failed");
     let cfg = MqttServerConfig {
         mqtt_socketaddr: Some("0.0.0.0:1883".parse().unwrap()),
+        keep_alive: 50
     };
     let server = MqttServer::new(cfg).await.unwrap();
     server.shutdown().await;
