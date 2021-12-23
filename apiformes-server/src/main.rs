71ce350988fd187a1d66e69bb83286e4534276d4
cgv6n3qy <apiformes@protonmail.com>
2021-12-23 10:18:48 +0000

    Make server run forever
    Signed-off-by: cgv6n3qy <apiformes@protonmail.com>

--- apiformes-server/src/main.rs
+++ apiformes-server/src/main.rs
@@ -6,8 +6,11 @@ async fn main() {
     tracing::subscriber::set_global_default(sub).expect("setting tracing default failed");
     let cfg = MqttServerConfig {
         mqtt_socketaddr: Some("0.0.0.0:1883".parse().unwrap()),
-        keep_alive: 50
+        keep_alive: 50,
     };
     let server = MqttServer::new(cfg).await.unwrap();
-    server.shutdown().await;
+    //server.shutdown().await;
+    loop {
+        tokio::task::yield_now().await;
+    }
 }
