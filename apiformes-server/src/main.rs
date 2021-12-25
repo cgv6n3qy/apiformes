f35d7c64245e5cb9b6d082586fd183de81fabb1b
cgv6n3qy <apiformes@protonmail.com>
2021-12-25 00:51:02 +0000

    Make server use environment vars for logging filtering
    Signed-off-by: cgv6n3qy <apiformes@protonmail.com>

--- apiformes-server/src/main.rs
+++ apiformes-server/src/main.rs
@@ -1,14 +1,20 @@
 use apiformes::server_async::{MqttServer, MqttServerConfig};
-use tracing_subscriber::FmtSubscriber;
+use tracing_subscriber::{filter::EnvFilter, FmtSubscriber};
+
 #[tokio::main]
 async fn main() {
-    let sub = FmtSubscriber::new();
+    let filter = EnvFilter::from_default_env(); //.add_directive(LevelFilter::INFO.into());
+    let sub = FmtSubscriber::builder()
+        .with_env_filter(filter)
+        .with_ansi(true)
+        .finish();
+
     tracing::subscriber::set_global_default(sub).expect("setting tracing default failed");
     let cfg = MqttServerConfig {
         mqtt_socketaddr: Some("0.0.0.0:1883".parse().unwrap()),
         keep_alive: 50,
     };
-    let server = MqttServer::new(cfg).await.unwrap();
+    let _server = MqttServer::new(cfg).await.unwrap();
     //server.shutdown().await;
     loop {
         tokio::task::yield_now().await;
