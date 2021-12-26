7318642306f29ab5931ecf40b8687b1076bb3cf1
cgv6n3qy <apiformes@protonmail.com>
2021-12-26 09:45:43 +0000

    Fix typo in noise protocol pattern
    Signed-off-by: cgv6n3qy <apiformes@protonmail.com>

--- apiformes/src/server_async/cfg.rs
+++ apiformes/src/server_async/cfg.rs
@@ -3,4 +3,4 @@ pub const TOPIC_ALIAS_MAX: u16 = 0;
 pub const WILDCARD_SUB: bool = false;
 pub const SUB_ID: bool = false;
 pub const SHARED_SUB: bool = false;
-pub const NOISE_PATTERN: &'static str = "Noise_NN_25519_ChaChaPoly_BLAKE2s";
+pub const NOISE_PATTERN: &'static str = "Noise_XK_25519_ChaChaPoly_BLAKE2s";
